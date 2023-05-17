fd881afb0eb9455e1ca3b0c3cae77aa5d74710f0
alessandro-casarini <alessandro.casarini92@gmail.com>
2023-05-18 00:21:13 +0200

    Modify Filter tool

--- SAPgrafici.py
+++ SAPgrafici.py
@@ -10,37 +10,42 @@ if uploaded_file is not None:
     dataframe = pd.read_excel(uploaded_file)
     st.title("Dashboard")
     
+    #Delete first two coloumns of excel. This use for selectbox to avoid 
+    #select "Time" and "Application Server" how filter will generate an error.
+
     list = dataframe.columns.values
     list = np.delete(list, [0,1,2], 0)
 
+    #Create filter widget
     selectedAppServer = st.sidebar.selectbox('Selected Application Server',dataframe['Application Server Instance'].unique())
-    selectedValueGraphics = st.sidebar.selectbox('Select value to calculate chart',list)
-    
-    #Filter pandas dataframe to obtain clean table
-    dfSelAppServer = dataframe[dataframe['Application Server Instance'] == selectedAppServer]
-    dfSelAppServer = dfSelAppServer[['Time', 'Application Server Instance', selectedValueGraphics]]
-    dfSelAppServer['Time'] = '10 Oct 2011 ' + dfSelAppServer['Time'].astype(str)
+    selectedValueGraphics = st.sidebar.multiselect('Select value to calculate chart',list)
 
-    #Plot result on vega lite chart
-    st.vega_lite_chart(dfSelAppServer, {
-    "width": 1000,
-    "autosize": {
-    "type": "fit",
-    "contains": "padding"
-    },
-    "height": 400,
-    "mark": "bar",
-    "encoding": {
-        "x": {
-            "timeUnit": "hoursminutesseconds",
-            "field": "Time",
-            "type": "ordinal",
-            "title": "Time",
+    for filter in selectedValueGraphics:
+        #Filter pandas dataframe to obtain clean table
+        dfSelAppServer = dataframe[dataframe['Application Server Instance'] == selectedAppServer]
+        dfSelAppServer = dfSelAppServer[['Time', 'Application Server Instance', filter]]
+        dfSelAppServer['Time'] = '10 Oct 2011 ' + dfSelAppServer['Time'].astype(str)
+
+        #Plot result on vega lite chart
+        st.vega_lite_chart(dfSelAppServer, {
+        "width": 1000,
+        "autosize": {
+        "type": "fit",
+        "contains": "padding"
         },
-        "y": {
-            "field": selectedValueGraphics,
-            "type": "quantitative",
-            "title": selectedValueGraphics, 
+        "height": 400,
+        "mark": "bar",
+        "encoding": {
+            "x": {
+                "timeUnit": "hoursminutesseconds",
+                "field": "Time",
+                "type": "ordinal",
+                "title": "Time",
+            },
+            "y": {
+                "field": filter,
+                "type": "quantitative",
+                "title": filter, 
+            },
         },
-    },
-    })
+        })
